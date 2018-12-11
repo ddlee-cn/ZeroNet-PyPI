49509340bb8937989bb4bd2afdea0345eca3791e
ddlee <cnlijiacheng96@gmail.com>
2018-12-11 22:38:58 +0800

    v0.1.4: multi-ver support
    Using Tox-travis to test lib on 3.6 and 3.7

--- zeronet/core/model.py
+++ zeronet/core/model.py
@@ -3,12 +3,7 @@
 # Docs:
 
 from __future__ import print_function
-try:
-    import pickle as pickle
-    import range
-else:
-    import cPickle as pickle
-    import xrange as range
+import pickle as pickle
 import os
 import numpy as np
 import zeronet.core.optimizer as optim
